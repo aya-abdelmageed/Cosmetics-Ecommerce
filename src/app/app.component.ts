c2a75320afc4cfb87867b526ba8b5f44c1cd9ec6
yasmeen elsayed <yasmeensaffan@gmail.com>
2025-05-07 18:59:42 +0300

    the category slide bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from "./Components/footer/footer.component";
+import { CategorySliderComponent } from "./Components/category-slider/category-slider.component";
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, RouterModule, HeaderComponent, FooterComponent] ,
+  imports: [CommonModule, RouterOutlet, RouterModule, HeaderComponent, FooterComponent,CategorySliderComponent] ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
