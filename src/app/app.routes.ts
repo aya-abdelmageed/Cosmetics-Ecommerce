32f1922c6f769423689c7b76dec6f9f93093c6b9
yasmeen elsayed <yasmeensaffan@gmail.com>
2025-05-07 14:58:55 +0300

    yasmeenversionofshop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from '../Guard/guards/auth.guard';
 import { AddProductComponent } from './Components/add-product/add-product.component';
 import { AdminGuard } from '../Guard/guards/admin.guard';
 import { NotAuthorizedComponent } from './Components/not-authorized/not-authorized.component';
+import { ShopComponent } from './Components/Shop/Shop.component';
 
 export const routes: Routes = [
    //{ path: '', component: HeaderComponent },
@@ -14,9 +15,7 @@ export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
    { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
    { path: 'add-product', component: AddProductComponent, canActivate: [AdminGuard] },
-   { path: 'not-authorized', component: NotAuthorizedComponent }
-
-  
-    
+   { path: 'not-authorized', component: NotAuthorizedComponent },
+   { path: 'Shop', component: ShopComponent }
   ];
   
